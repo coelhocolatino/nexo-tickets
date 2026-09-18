e0f963b33747e2eb016d36a73dd87cb26e086556
WANDERSON COELHO <colatino.ventas.enlinea@gmail.com>
2026-09-18 18:12:26 +0200

    Update cache name to nexo-tickets-v18

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ==============================
 // Nexo · SAD Tickets - Service Worker v5
 // ==============================
-const CACHE_NAME = "nexo-tickets-v17";
+const CACHE_NAME = "nexo-tickets-v18";
 
 const ASSETS = [
   "./",
